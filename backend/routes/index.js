0eb48fac659068dd2064cb7a610df91c010ae825
Eric Moore <4chrise8@gmail.com>
2025-07-16 17:49:27 -0700

    Server now handles error when 429 is thrown

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 const pdfParse = require('pdf-parse');
 var router = express.Router();
 
-
 const upload = multer({ dest: 'uploads/' });
 
 /* GET home page. */
@@ -20,31 +19,62 @@ router.post('/', upload.single('resume'), async (req, res, next) => {
   if (!jobDesc) return res.status(400).json({ 'error': 'jobDesc is required.' });
   if (!resume) return res.status(400).json({ 'error': 'resume is required.' });
 
-  let response = await getHisInput(jobDesc, resume);
+  try {
+    let response = await getHisInput(jobDesc, resume);
 
-  response = response.substring(7, response.length - 4);
-  response = JSON.parse(response);
+    response = response.substring(7, response.length - 4);
+    response = JSON.parse(response);
 
-  return res.json({ response });
+    return res.json({ response });
+  } catch (err) {
+    console.error(err);
+    if (err.status === 429)
+      return res.status(503).json({ message: 'Server rate limit reached.' });
+    else
+      return res.status(500).json({ message: 'An error occurred.' });
+  }
 });
 
+/**
+ * Extracts the text data from the given pdf
+ * @param {Express.Multer.File} pdf the pdf from which to extract
+ * @returns the text data from the pdf
+ */
+const extractPdf = async (pdf) => {
+  if (!pdf) throw 'Cannot parse file to be extracted.';
+  try {
+    const pdfBuffer = fs.readFileSync(pdf.path);
+    const data = await pdfParse(pdfBuffer);
+    const extractedText = data.text;
+    return extractedText;
+  } catch (err) {
+    throw err;
+  }
+}
+
+/**
+ * Queries the AI to respond to the given job and candidate infomation.
+ * @param {String} jobDesc the job description text
+ * @param {*} resume the resume file in PDF form
+ * @returns the AI response
+ */
 const getHisInput = async (jobDesc, resume) => {
-  const openai = new OpenAI();
 
-  // Read and parse the PDF
-  const pdfBuffer = fs.readFileSync(resume.path);
-  const data = await pdfParse(pdfBuffer);
-  const extractedText = data.text;
+  try {
+    const openai = new OpenAI();
 
-  // Cleanup - remove file after processing
-  fs.unlinkSync(resume.path);
+    // Read and parse the PDF
+    const extractedText = await extractPdf(resume);
 
-  const completion = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-      {
-        role: "system",
-        content: `
+    // Cleanup - remove file after processing
+    fs.unlinkSync(resume.path);
+
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content: `
           Assume the role of a hiring manager.
           Provided by the user is the job description of a company position and the content of a resume from a prospective candidate.
           How likely are you (on a scale of 1 to 10) to hire this candidate for this position compared to the average likely candidate? Explain your reasoning.
@@ -70,19 +100,22 @@ const getHisInput = async (jobDesc, resume) => {
         }
           """
           `
-      },
-      {
-        role: "user",
-        content: `Job Description: ${jobDesc}`,
-      },
-      {
-        role: "user",
-        content: `Resume: ${extractedText}`,
-      },
-    ]
-  });
-  const result = completion.choices[0].message?.content;
-  return result;
+        },
+        {
+          role: "user",
+          content: `Job Description: ${jobDesc}`,
+        },
+        {
+          role: "user",
+          content: `Resume: ${extractedText}`,
+        },
+      ]
+    });
+    const result = completion.choices[0].message?.content;
+    return result;
+  } catch (err) {
+    throw err;
+  }
 }
 
 module.exports = router;
